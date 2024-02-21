54fefca3089765ee582e55800bc2aecdd4f96aa4
Carlos Méndez <cmendez@doyoukare.com>
2024-02-20 20:51:58 -0600

    Refactor card types and add new card types

--- lib/screens/cards/cards_screen.dart
+++ lib/screens/cards/cards_screen.dart
@@ -3,13 +3,9 @@ import 'package:flutter/material.dart';
 const cardsList = <Map<String, dynamic>>[
   {'elevation': 0.0, 'label': 'Elevation 0'},
   {'elevation': 1.0, 'label': 'Elevation 1'},
-  {'elevation': 2.0, 'label': 'Elevation 2'},
   {'elevation': 3.0, 'label': 'Elevation 3'},
-  {'elevation': 4.0, 'label': 'Elevation 4'},
   {'elevation': 6.0, 'label': 'Elevation 6'},
-  {'elevation': 8.0, 'label': 'Elevation 8'},
   {'elevation': 12.0, 'label': 'Elevation 12'},
-  {'elevation': 16.0, 'label': 'Elevation 16'},
 ];
 
 class CardsScreen extends StatelessWidget {
@@ -35,7 +31,20 @@ class _CardsView extends StatelessWidget {
           ...cardsList
               .map((card) => _CardType1(
                   elevation: card['elevation'], label: card['label']))
-              .toList()
+              .toList(),
+          ...cardsList
+              .map((card) => _CardType2(
+                  elevation: card['elevation'], label: card['label']))
+              .toList(),
+          ...cardsList
+              .map((card) => _CardType3(
+                  elevation: card['elevation'], label: card['label']))
+              .toList(),
+          ...cardsList
+              .map((card) => _CardType4(
+                  elevation: card['elevation'], label: card['label']))
+              .toList(),
+          const SizedBox(height: 50)
         ],
       ),
     );
@@ -72,6 +81,129 @@ class _CardType1 extends StatelessWidget {
               ),
             ],
           ),
+        ));
+  }
+}
+
+class _CardType2 extends StatelessWidget {
+  final String label;
+  final double elevation;
+
+  const _CardType2({
+    required this.label,
+    required this.elevation,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+    return Card(
+        shape: RoundedRectangleBorder(
+          borderRadius: const BorderRadius.all(Radius.circular(12)),
+          side: BorderSide(
+            color: colors.outline,
+          ),
+        ),
+        elevation: elevation,
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(10, 5, 10, 10),
+          child: Column(
+            children: <Widget>[
+              Align(
+                alignment: Alignment.topRight,
+                child: IconButton(
+                  onPressed: () {},
+                  icon: const Icon(Icons.more_vert),
+                ),
+              ),
+              Align(
+                alignment: Alignment.topLeft,
+                child: Text('$label - outline'),
+              ),
+            ],
+          ),
+        ));
+  }
+}
+
+class _CardType3 extends StatelessWidget {
+  final String label;
+  final double elevation;
+
+  const _CardType3({
+    required this.label,
+    required this.elevation,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+
+    return Card(
+        color: colors.surfaceVariant,
+        elevation: elevation,
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(10, 5, 10, 10),
+          child: Column(
+            children: <Widget>[
+              Align(
+                alignment: Alignment.topRight,
+                child: IconButton(
+                  onPressed: () {},
+                  icon: const Icon(Icons.more_vert),
+                ),
+              ),
+              Align(
+                alignment: Alignment.topLeft,
+                child: Text('$label - filled'),
+              ),
+            ],
+          ),
+        ));
+  }
+}
+
+class _CardType4 extends StatelessWidget {
+  final String label;
+  final double elevation;
+
+  const _CardType4({
+    required this.label,
+    required this.elevation,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+        clipBehavior: Clip.hardEdge,
+        elevation: elevation,
+        child: Stack(
+          children: <Widget>[
+            Image.network(
+              'https://picsum.photos/id/${elevation.toInt()}/600/250',
+              fit: BoxFit.cover,
+              height: 350,
+            ),
+            Align(
+              alignment: Alignment.topRight,
+              child: Container(
+                decoration: const BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(20),
+                  ),
+                ),
+                child: IconButton(
+                  onPressed: () {},
+                  icon: const Icon(Icons.more_vert),
+                ),
+              ),
+            ),
+            Align(
+              alignment: Alignment.topLeft,
+              child: Text('$label - image'),
+            ),
+          ],
         ));
   }
 }
