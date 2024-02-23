677bcde679293f998a54615e7980690f015879f7
Carlos Méndez <cmendez@doyoukare.com>
2024-02-22 21:24:41 -0600

    Add infinite scroll functionality and pull-to-refresh feature

--- lib/screens/infinite_scroll_screen.dart
+++ lib/screens/infinite_scroll_screen.dart
@@ -1,3 +1,4 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
@@ -9,16 +10,48 @@ class InfiniteScrollScreen extends StatefulWidget {
 }
 
 class _InfiniteScrollScreenState extends State<InfiniteScrollScreen> {
-  List<int> imagesIds = [1, 2, 3, 4, 5, 6];
   final ScrollController scrollController = ScrollController();
+  List<int> imagesIds = [1, 2, 3, 4, 5, 6];
+  bool isLoading = false;
+  bool isMounted = true;
 
-  void addMoreImages() {
+  Future addMoreImages() async {
+    if (isLoading) return;
+    isLoading = true;
+    setState(() {});
+    await Future.delayed(const Duration(seconds: 2));
     final lastId = imagesIds.last;
     for (int i = 1; i <= 6; i++) {
       imagesIds.add(lastId + i);
     }
+    isLoading = false;
+    if (!isMounted) return;
     setState(() {});
-    print(imagesIds);
+    moveScrollToButtom();
+  }
+
+  Future<void> onRefresh() async {
+    isLoading = true;
+    setState(() {});
+    await Future.delayed(const Duration(seconds: 3));
+    final lastId = imagesIds.last;
+    imagesIds.clear();
+    for (int i = 1; i <= 6; i++) {
+      imagesIds.add(lastId + i);
+    }
+    isLoading = false;
+    if (!isMounted) return;
+    setState(() {});
+  }
+
+  void moveScrollToButtom() {
+    if (scrollController.position.pixels <=
+        scrollController.position.maxScrollExtent - 100) return;
+    scrollController.animateTo(
+      scrollController.position.pixels + 200,
+      duration: const Duration(milliseconds: 500),
+      curve: Curves.fastOutSlowIn,
+    );
   }
 
   @override
@@ -35,6 +68,7 @@ class _InfiniteScrollScreenState extends State<InfiniteScrollScreen> {
   @override
   void dispose() {
     scrollController.dispose();
+    isMounted = false;
     super.dispose();
   }
 
@@ -46,24 +80,35 @@ class _InfiniteScrollScreenState extends State<InfiniteScrollScreen> {
           context: context,
           removeTop: true,
           removeBottom: true,
-          child: ListView.builder(
-            controller: scrollController,
-            itemCount: imagesIds.length,
-            itemBuilder: (context, index) {
-              return FadeInImage(
-                fit: BoxFit.cover,
-                width: double.infinity,
-                height: 300,
-                placeholder: const AssetImage('assets/images/jar-loading.gif'),
-                image: NetworkImage(
-                    'https://picsum.photos/id/${imagesIds[index]}/500/300'),
-              );
-            },
+          child: RefreshIndicator(
+            onRefresh: onRefresh,
+            edgeOffset: 10,
+            strokeWidth: 2,
+            child: ListView.builder(
+              controller: scrollController,
+              itemCount: imagesIds.length,
+              itemBuilder: (context, index) {
+                return FadeInImage(
+                  fit: BoxFit.cover,
+                  width: double.infinity,
+                  height: 300,
+                  placeholder:
+                      const AssetImage('assets/images/jar-loading.gif'),
+                  image: NetworkImage(
+                      'https://picsum.photos/id/${imagesIds[index]}/500/300'),
+                );
+              },
+            ),
           ),
         ),
         floatingActionButton: FloatingActionButton(
           onPressed: () => context.pop(),
-          child: const Icon(Icons.arrow_back_ios_new_outlined),
+          child: isLoading
+              ? SpinPerfect(
+                  infinite: true,
+                  child: const Icon(Icons.refresh),
+                )
+              : FadeIn(child: const Icon(Icons.arrow_back_ios_rounded)),
         ));
   }
 }
